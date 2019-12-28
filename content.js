2d5dc12db0b08ef394bfa5f3dc45d276771aa299
Jesse Kela <jessekkela@gmail.com>
2019-12-28 14:18:21 +0200

    Works now on specific site and url only

--- content.js
+++ content.js
@@ -1,8 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.message === "clicked_browser_action") {
-            var url = request.data + "media/?size=l";
-            chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
+        
+            var url = request.data.toString()
+            if (url.startsWith("https://www.instagram.com/p/"))
+            {
+                var url = request.data + "media/?size=l";
+                chrome.runtime.sendMessage({"message": "open_new_tab", "url": url});
+            }
         }
     }
 );
