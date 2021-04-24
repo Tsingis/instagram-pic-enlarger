2018c233d3d266d3e6c90acd1b305ad2b37f27ab
Jesse Kela <jessekkela@gmail.com>
2021-04-24 14:51:12 +0300

    Added semicolons

--- background.js
+++ background.js
@@ -1,24 +1,24 @@
 chrome.action.onClicked.addListener(async () => {
-    let activeTab = await getActiveTab()
+    let activeTab = await getActiveTab();
     let msg = {
         "command": "clicked_browser_action",
         "data": activeTab.url
-    }
+    };
     chrome.tabs.sendMessage(activeTab.id, msg);
-})
+});
 
 chrome.runtime.onMessage.addListener(
     (request, sender, response) => {
         if (request.command === "open_new_tab") {
-            chrome.tabs.create({ "url": request.data })
+            chrome.tabs.create({ "url": request.data });
         }
-    })
+    });
 
 const getActiveTab = async () => {
     let queryOptions = {
         active: true,
         currentWindow: true
-    }
-    let [activeTab] = await chrome.tabs.query(queryOptions)
-    return activeTab
-}
+    };
+    let [activeTab] = await chrome.tabs.query(queryOptions);
+    return activeTab;
+};
--- content.js
+++ content.js
@@ -1,21 +1,21 @@
 chrome.runtime.onMessage.addListener(
     (request, sender, response) => {
         if (request.command === "clicked_browser_action") {
-            let url = request.data.toString()
+            let url = request.data.toString();
             if (url.startsWith("https://www.instagram.com/p/")) {
                 msg = {
                     "command": "open_new_tab",
                     "data": getLargePictureUrl(url)
-                }
+                };
                 chrome.runtime.sendMessage(msg);
             }
         }
     }
-)
+);
 
 const getLargePictureUrl = (url) => {
     if (url.match(/\//g).length > 4) {
-        url = url.substr(0, url.lastIndexOf("\/"))
+        url = url.substr(0, url.lastIndexOf("\/"));
     }
-    return url + "/media/?size=l"
-}
+    return url + "/media/?size=l";
+};
