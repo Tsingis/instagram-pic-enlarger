8dbfb63f0ba328ae58b2f25736b3874dab189a85
Jesse Kela <jessekkela@gmail.com>
2025-04-18 21:23:58 +0300

    Use nullish coalescing operator

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -20,10 +20,7 @@ export const test = base.extend<{
   },
   extensionId: async ({ context }: any, use: any) => {
     let [background] = context.serviceWorkers()
-    if (!background) {
-      background = await context.waitForEvent("serviceworker")
-    }
-
+    background ??= await context.waitForEvent("serviceworker")
     const extensionId = background.url().split("/")[2]
     await use(extensionId)
   },
